026f45bc389b33f7d95bd0e3264337c479423e6c
Emilia Trentham <lavernatrentham@gmail.com>
2022-12-13 23:01:00 -0800

    Add delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,50 @@
+// Dependencies
 const express = require("express");
-const notes = express.Router();
-const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const router = express.Router();
+const { readFromFile, writeToFile, readAndAppend, } = require("../helpers/fsUtils");
+
+// Allows for unique ids to be created
 const uuid = require("../helpers/uuid");
 
-// GET Route for retrieving all the notes
-notes.get("/", (req, res) => {
-  console.info(`${req.method} request received for notes`);
-  readFromFile("../db/db.json").then((data) => res.json(JSON.parse(data)));
+
+
+// GET Route for retrieving and returning all saved notes as JSON
+router.get("/notes", (req, res) => {
+  console.info(`${req.method} `);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new UX/UI tip
-notes.post("/", (req, res) => {
-  console.info(`${req.method} request received to add a tip`);
+// POST Route for a new note
+router.post("/notes", (req, res) => {
+  console.info(`${req.method}`);
   console.log(req.body);
 
-  const { username, topic, tip } = req.body;
+  const {title, text} = req.body;
+   if (req.body) {
+     const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
 
-//   if (req.body) {
-//     const newTip = {
-//       username,
-//       tip,
-//       topic,
-//       tip_id: uuid(),
-//     };
-
-//     readAndAppend(newTip, "../db/db.json");
-//     res.json(`Tip added successfully 🚀`);
-//   } else {
-//     res.error("Error in adding tip");
-//   }
+     readAndAppend(newNote, "./db/db.json");
+     console.log(newNote);
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error("Error in adding Note");
+  }
 });
 
-module.exports = notes;
+// Deletes note with specific id
+router.delete('/notes/:uuid', async (req, res) => {
+  const uuid = req.params.uuid;
+  const existingData = await readFromFile("./db/db.json");
+  const jsonData = JSON.parse(existingData);
+  const filteredData = jsonData.filter((data) => {
+    return data.id !== uuid;
+  })
+  writeToFile('./db/db.json', filteredData);
+  res.json('Success!');
+});
+
+module.exports = router;
